026e2c0f3454c3c33c98f2f64ab52ff0cc8cba7a
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-02-05 22:48:56 +0100

    Building client and visits

---
+++ src/main/java/API/Distance.java
@@ -0,0 +1,7 @@
+package API;
+
+public class Distance {
+
+    //Implement the google maps api to be able to calculate the distance needed between the visits.
+
+}
--- src/main/java/clients/Client.java
+++ src/main/java/clients/Client.java
@@ -1,14 +1,27 @@
 package clients;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Client {
 
 
+    private Long id; //Primary key?
     private String name;
     private String address;
     private List<Visits> visits = new ArrayList<>();
+
+
+    public Client () {
+        this.name = name;
+        this.address = address;
+        this.visits = visits;
+    }
 
     public Client(String name, String address) {
         this.name = name;
@@ -19,4 +32,12 @@ public class Client {
         visits.add(new Visits(type, startingTime, length)); //Kan man göra så här??
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
